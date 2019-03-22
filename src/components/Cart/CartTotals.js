5f35134767bc1bff4beffe538e53b013b7507977
nignanthomas <nignanthomas@gmail.com>
2019-03-22 16:59:05 +0300

    add paypal payment system

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PayPalButton from './PayPalButton';
 
-const CartTotals = ({value}) => {
+const CartTotals = ({value,history}) => {
 
     const {cartSubTotal,cartTax,cartTotal,clearCart}= value;
 
@@ -14,6 +15,10 @@ const CartTotals = ({value}) => {
                         <h5><span className="text-title">subtotal: </span> <strong>${cartSubTotal}</strong></h5>
                         <h5><span className="text-title">tax: </span> <strong>${cartTax}</strong></h5>
                         <h5><span className="text-title">total: </span> <strong>${cartTotal}</strong></h5>
+
+                        <PayPalButton total={cartTotal} clearCart={clearCart} history={history}>
+
+                        </PayPalButton>
                     </div>
                 </div>
             </div>
